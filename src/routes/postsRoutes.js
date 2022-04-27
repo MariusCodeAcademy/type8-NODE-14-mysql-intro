57975912a5a4a42f09b168ac598cf2afcbedbc0c
Marius <marius@example.com>
2022-04-27 12:05:10 +0300

    postUzd

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -43,5 +43,28 @@ postRoutes.get('/first-posts', async (req, res) => {
 // sukurti 404 endpointa kuris grazintu atsakyma json formatu
 
 // GET /api/posts/name/:name - parisiusti postus kuriuos parase :name
+postRoutes.get('/posts/name/:name', async (req, res) => {
+  let conn;
+  try {
+    const { name } = req.params;
+    console.log('name ===', name);
+    conn = await mysql.createConnection(dbConfig);
+    // esam neapsaugoti nuo sql injection
+    // const sql = `SELECT * FROM posts WHERE author = '${name}'`;
+    // const [rows] = await conn.query(sql);
+
+    // reikia neautralizuoti vartotojo ivesties duomenis
+    const sql = 'SELECT * FROM posts WHERE author = ?';
+    const [rows] = await conn.execute(sql, [name]);
+    res.json(rows);
+  } catch (error) {
+    console.log('error getting all posts', error);
+    res.sendStatus(500);
+  } finally {
+    await conn?.end();
+  }
+});
+
+// POST /api/posts - sukurti nauja posta su duomenimis kuriuos gavo is post route
 
 module.exports = postRoutes;

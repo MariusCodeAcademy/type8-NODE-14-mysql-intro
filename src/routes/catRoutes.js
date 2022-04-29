6ec79616a1b4473aca9a3b2c47e01f98570db4ea
Marius <marius@example.com>
2022-04-29 10:35:08 +0300

    mvc create categories select

--- src/routes/catRoutes.js
+++ src/routes/catRoutes.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
+const { dbConfig } = require('../config');
 
 const catRoutes = express.Router();
 
-async function createTableDB() {}
+// model fn
+async function createTableDB() {
+  console.log('createTableDB model ran');
+  let conn;
+  try {
+    conn = await mysql.createConnection(dbConfig);
+    const sql = 'SELECT * FROM posts';
+    const [result] = await conn.query(sql);
+    return result;
+  } catch (error) {
+    console.log('error createTableDB', error);
+    return false;
+  } finally {
+    conn?.end();
+  }
+}
 
 async function createTable(req, res) {
-  const success = createTableDB();
-  if (success) res.json('table created');
+  console.log('createTable controller ran');
+  const createResult = await createTableDB();
+
+  if (createResult === false) {
+    res.sendStatus(500);
+    return;
+  }
+  res.json(createResult);
 }
 
 // routes

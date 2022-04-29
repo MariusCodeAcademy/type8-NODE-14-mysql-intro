c6077ff110b41919d3ae5729c36ccb861005da8c
Marius <marius@example.com>
2022-04-29 11:47:29 +0300

    create table

--- src/models/catModel.js
+++ src/models/catModel.js
@@ -7,7 +7,14 @@ async function createTableDB() {
   let conn;
   try {
     conn = await mysql.createConnection(dbConfig);
-    const sql = 'SELECT * FROM posts';
+    const sql = `
+    CREATE TABLE type8_first_db.categories ( 
+      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      title VARCHAR(255) NOT NULL, 
+      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+      PRIMARY KEY (id)
+      ) ENGINE = InnoDB;
+    `;
     console.log('pries uzklausa');
     const [result] = await conn.query(sql);
     console.log('po uzklausos');

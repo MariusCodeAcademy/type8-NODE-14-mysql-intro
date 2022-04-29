4d211d890b4ccb3c81ccf944d6bdd967571c2bcb
Marius <marius@example.com>
2022-04-29 10:05:29 +0300

    mvc example

--- src/routes/catRoutes.js
+++ src/routes/catRoutes.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const catRoutes = express.Router();
 
+async function createTableDB() {}
+
+async function createTable(req, res) {
+  const success = createTableDB();
+  if (success) res.json('table created');
+}
+
 // routes
-catRoutes.post('/categories/create', async (req, res) => {
-  res.json('create category');
-});
+catRoutes.post('/categories/create', createTable);
 
 module.exports = catRoutes;
